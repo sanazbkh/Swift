e5712a3a1667457132481ba96b6b51f5c58033d9
sanaz Bahmankhah <Sanaz.bahmankhah@tekever.com>
2022-12-01 18:10:54 +0100

    Scheduled local notification.

--- Notification/Notification/ViewController.swift
+++ Notification/Notification/ViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    private let center = UNUserNotificationCenter.current()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -16,8 +18,23 @@ class ViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
     
-    @objc func registerLocal() {}
+    @objc func registerLocal() {
+        self.center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("User gave permission")
+            } else {
+                print("permission denied")
+            }
+        }
+    }
     
-    @objc func scheduleLocal() {}
+    @objc func scheduleLocal() {
+        let content = UNMutableNotificationContent()
+        content.title = "woman life freedom"
+        content.body = "Do not doubt.. we are victorious"
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5 , repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        self.center.add(request)
+    }
 }
 
